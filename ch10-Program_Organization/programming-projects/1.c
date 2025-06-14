b9ed2411070b22f8adedde92d740daf84fbf538a
supervincii <74002397+supervincii@users.noreply.github.com>
2025-06-15 02:00:15 +0800

    fix project 1 ch10

--- ch10-Program_Organization/programming-projects/1.c
+++ ch10-Program_Organization/programming-projects/1.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 100
 
@@ -8,20 +9,40 @@ int contents[STACK_SIZE];
 int top = 0;
 
 // prototypes
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
 void push(char i);
 char pop(void);
 void stack_overflow(void);
 void stack_underflow(void);
 
 int main(void) {
-    char ch;
-    int i = 0;
+    char ch, d;
 
     printf("Enter parentheses and/or braces: ");
+
     while ((ch = getchar()) != '\n') {
-        if (ch == '{' || ch == '(')
+        if (ch == '}' && pop() != '{') {
+            printf("Parentheses/braces are not nested properly.\n");
+            return 0;          
+        }
+        else if (ch == ')' && pop() != '(') {
+            printf("Parentheses/braces are not nested properly.\n");
+            return 0;          
+        }
+        else if (ch == '(' || ch == '{') {
             push(ch);
+        }
     }
+
+    if (!is_empty()) {
+        printf("Parentheses/braces are not nested properly.\n");
+        return 0;
+    }
+
+    printf("Parentheses/braces are nested properly\n");
+    return 0;
 }
 
 void make_empty(void) { top = 0; }
@@ -42,4 +63,14 @@ char pop(void) {
         stack_underflow();
     else
         return contents[--top];
+}
+
+void stack_overflow(void) {
+    printf("\nStack overflow\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void) {
+    printf("\nStack underflow\n");
+    exit(EXIT_FAILURE);
 }
