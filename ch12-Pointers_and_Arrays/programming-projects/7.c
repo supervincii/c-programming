a4b977f9650c43afac1b4ccd541d2f0f39a33b39
supervincii <74002397+supervincii@users.noreply.github.com>
2025-06-24 22:28:50 +0800

    complete ch12 programming projects

---
+++ ch12-Pointers_and_Arrays/programming-projects/7.c
@@ -0,0 +1,38 @@
+/* Finds the largest and smallest elements in an array */
+
+#include <stdio.h>
+
+#define N 10
+
+void max_min(int a[], int n, int *max, int *min);
+
+int main(void) {
+    int b[N], i, big, small;
+
+    printf("Enter %d numbers: ", N);
+    for (i = 0; i < N; i++)
+        scanf("%d", &b[i]);
+
+    // &big and &small are addresses to big and small respectively
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
+
+    return 0;
+}
+
+void max_min(int a[], int n, int *max, int *min) {
+    int *p;
+
+    // max and min points to the address of big and small. In this
+    // case, we use the indirection operator on max and min to get
+    // the value of big and small, not their addresses.
+    *max = *min =  a[0];
+    for (p = a + 1; p < a + n; p++) {
+        if (*p > *max)
+            *max = *p;
+        else if (*p < *min)
+            *min = *p;
+    }
+}
